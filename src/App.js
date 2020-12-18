314841765f42e9b52441ff21a9ffdc2e66a762ae
Mohammad <mohammad.alhallaq1@gmail.com>
2020-12-18 19:03:28 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+function App() {
+
+
+  const api = {
+    key: "f3c814fff2f08084faa2817a11c1b329",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+
+  const [weather, setWeather] = useState({});
+  const [query, setQuery] = useState('');
+
+  const search = ev =>{
+    if(ev.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&appid=${api.key}&units=metric`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result)
+        console.log(result)
+        setQuery('')
+      })
+    }
+  }
+
+
+
+  const dateBuilder = (d) =>{
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
+
+  return (
+    <div className={(typeof weather.main != 'undefined') ? ((weather.main.temp > 16) ? 'app warm' : 'app'):'app'}>
+      <main>
+        <div className="search-box">
+          <input className="search-bar"
+           type="text" 
+           placeholder="search..." 
+           onChange={e => setQuery(e.target.value)}
+           value={query}
+           onKeyPress={search}/>
+        </div>
+
+        {(typeof weather.main != 'undefined') ? (
+        <div>
+          <div className='location-box'>
+          <div className='location'>{weather.name}, {weather.sys.country}</div>
+          <div className='date'>{dateBuilder(new Date())}</div>
+        </div>
+        <div className='weather-box'>
+        <div className='temp'>{Math.round(weather.main.temp)}</div>
+        <div className='weather'>{weather.weather[0].main}</div>
+        </div>
+        </div>
+        ) : ('')}
+      </main>
+    </div>
+  );
+}
+
+export default App;
